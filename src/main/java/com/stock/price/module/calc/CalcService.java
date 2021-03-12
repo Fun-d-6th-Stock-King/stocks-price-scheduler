2fba78cce550c0869a2d9651a1db4d6b5051b351
박기현[메가존] <t2006036@partner.coway.co.kr>
2021-03-12 15:44:56 +0900

    test: 종목 전년도 데이터 조회

--- src/main/java/com/stock/price/module/calc/CalcService.java
+++ src/main/java/com/stock/price/module/calc/CalcService.java
@@ -27,28 +27,28 @@ public class CalcService {
 
 	@Autowired
 	private StocksRepository stockRepository;
-	
+
 	@Autowired
 	private StocksPriceRepository stocksPriceRepository;
 
-//	@Scheduled(cron = "0 15 10 15 * ?", zone = "Asia/Seoul")
+	//	@Scheduled(cron = "0 15 10 15 * ?", zone = "Asia/Seoul")
 	@Scheduled(fixedRate = 10000000) // 테스트용
 	public List<Stocks> getStockList() throws Exception {
 		List<Stocks> stockList = stockRepository.findAllByMarket("KS")
 			.orElseThrow(() -> new Exception("종목이 조회되지 않습니다."));
 
 		Stocks target = stockList.get(0);
-		
-		String code = target.getCode() + ".1" + target.getMarket();
-		
+
+		String code = target.getCode() + "." + target.getMarket();
+
 		log.info(code);
-		
+
 		LocalDate now = LocalDate.now();
 
 		Stock stock = YahooFinance.get(code, true);
-		
+
 		BigDecimal price = stock.getQuote().getPrice();
-		
+
 		System.out.println(price);
 
 		List<HistoricalQuote> hist = getHistory(stock, now.plusYears(-1), now.plusYears(-1).plusDays(7),
@@ -56,15 +56,16 @@ public class CalcService {
 
 		if (!hist.isEmpty()) {
 			HistoricalQuote year1 = hist.get(0);
-			LocalDateTime date =  convertDateTime(year1.getDate());
+			LocalDateTime date = convertDateTime(year1.getDate());
 			BigDecimal oldPrice = year1.getClose();
 			BigDecimal yield = price
 				.subtract(oldPrice)
 				.divide(oldPrice, MathContext.DECIMAL32)
-				.multiply(new BigDecimal(100))
-				.setScale(2, RoundingMode.HALF_EVEN);
+				.multiply(new BigDecimal(10000));
 			System.out.println("1년 전 [" + date + "] " + oldPrice + " / " + yield);
-			StocksPrice stocksPrice = new StocksPrice();
+			StocksPrice stocksPrice = stocksPriceRepository
+				.findByStocksId(target.getId()).orElse(new StocksPrice());
+
 			stocksPrice.setStocksId(target.getId());
 			stocksPrice.setPrice(stock.getQuote().getPrice());
 			Calendar dateCheck = stock.getHistory().get(stock.getHistory().size() - 1).getDate();
@@ -78,20 +79,21 @@ public class CalcService {
 
 		return stockList;
 	}
-	
+
 	public static Calendar convertCal(LocalDate localDate) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.clear();
 		calendar.set(localDate.getYear(), localDate.getMonthValue() - 1, localDate.getDayOfMonth());
 		return calendar;
 	}
-	
+
 	public static LocalDateTime convertDateTime(Calendar calendar) {
 		return LocalDateTime.ofInstant(calendar.toInstant(), ZoneId.of("Asia/Seoul"));
 	}
-	
-	public static List<HistoricalQuote> getHistory(Stock stock, LocalDate from, LocalDate to, Interval interval) throws IOException{
-		
+
+	public static List<HistoricalQuote> getHistory(Stock stock, LocalDate from, LocalDate to, Interval interval)
+		throws IOException {
+
 		return stock.getHistory(convertCal(from), convertCal(to), interval);
 	}
 }
--- src/main/java/com/stock/price/module/calc/StocksPriceRepository.java
+++ src/main/java/com/stock/price/module/calc/StocksPriceRepository.java
@@ -1,9 +1,12 @@
 package com.stock.price.module.calc;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StocksPriceRepository extends JpaRepository<StocksPrice, Integer> {
 
+	public Optional<StocksPrice> findByStocksId(int stocksId);
 }
